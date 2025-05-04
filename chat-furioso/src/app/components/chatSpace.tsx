dac6fbca9bf7c42f852063e18454573d48ba4fd3
Breno Amorim <brenoamorim11@gmail.com>
2025-05-03 21:27:14 -0300

    fixing chat always on bottom when message is added

--- chat-furioso/src/app/components/chatSpace.tsx
+++ chat-furioso/src/app/components/chatSpace.tsx
@@ -1,9 +1,21 @@
+import { useEffect, useRef } from 'react'
 import { MessageList } from '../types/message'
 import ChatMessage from './chatMessages/chatMessage'
 
 export default function ChatSpace({ message }: MessageList) {
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const container = containerRef.current
+    if (container) {
+      container.scrollTop = container.scrollHeight
+    }
+  }, [message]) 
+
   return (
-    <div className="scrollbar-none h-min w-full flex-col-reverse items-end space-y-8 overflow-y-auto pb-4">
+    <div
+    ref={containerRef}
+    className="scrollbar-none h-min w-full flex-col-reverse items-end space-y-8 overflow-y-auto pb-4">
       {message.map((message, index) => {
         return (
           <ChatMessage
